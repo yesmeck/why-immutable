798c0b3b551777fd461624fb0dde3de1c04616d2
Wei Zhu <yesmeck@gmail.com>
2015-11-14 01:44:37 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [
+        {
+          name: "Bread",
+        },
+        {
+          name: "Butterfly",
+        },
+        {
+          name: "Milk"
+        }
+      ],
+      aha: false,
+    };
+  }
+
+  handleClick() {
+    this.setState({aha: true})
+  }
+
+  render() {
+    let { items } = this.state;
+    // items = items.slice(); // Unless we clone a new items array.
+    items[0].name = "Cookie";
+
+    return (
+      <div>
+        <button onClick={::this.handleClick}>CLICK ME</button>
+        <ShoppingList items={items} />
+      </div>
+    );
+  }
+}
+
+class ShoppingList extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (this.props.items !== nextProps.items) {
+      alert('Aha!'); // This line will never be called.
+    }
+  }
+
+  render() {
+    const { items } = this.props;
+
+    return (
+      <ul>
+        {
+          (items.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          )))
+        }
+      </ul>
+    );
+  }
+}
+
+ReactDOM.render(<App />, window.app);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['react', 'es2015', 'stage-0'] } },
+    ]
+  },
+  progress: true,
+  resolve: {
+    modulesDirectories: [
+      'src',
+      'node_modules'
+    ],
+    extensions: ['', '.json', '.js']
+  }
+};
