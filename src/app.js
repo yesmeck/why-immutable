89989ed3cc17992c07511893e9e82ee8b27f121a
Wei Zhu <yesmeck@gmail.com>
2015-11-14 01:59:14 +0800

    Where is my bread?

--- src/app.js
+++ src/app.js
@@ -20,9 +20,11 @@ class App extends Component {
   }
 
   render() {
-    let { items } = this.state;
-    // items = items.slice(); // Unless we clone a new items array.
-    items[0].name = "Cookie";
+    let { items, aha } = this.state;
+    if (aha) {
+      // items = items.slice(); // Unless we clone a new items array.
+      items[0].name = "Cookie";
+    }
 
     return (
       <div>
@@ -36,7 +38,7 @@ class App extends Component {
 class ShoppingList extends Component {
   componentWillReceiveProps(nextProps) {
     if (this.props.items !== nextProps.items) {
-      alert('Aha!'); // This line will never be called.
+      alert('Where is my bread?'); // This line will never be called.
     }
   }
 
